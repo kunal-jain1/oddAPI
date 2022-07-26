783b58dfb61af1c6a7a37973db41d6568bdb8ad1
Kunal Jain <55034053+wherewith@users.noreply.github.com>
2022-07-25 23:01:07 -0500

    re-add stuff

---
+++ main.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+import json
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template("home.html")
+
+@app.route('/<string:num>', methods=['GET'])
+def get_num(num):
+    int_num = int(num)
+    num_boolean = None
+    if (int_num % 2) == 0:
+        num_boolean = False
+    else:
+        num_boolean = True
+    return json.dumps(num_boolean)
+
+if __name__ == "__main__":
+    app.run(debug=True)
